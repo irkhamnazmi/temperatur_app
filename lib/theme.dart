6c7b2846553ecc75d64e81d2fb5461fb98dd8c97
Irkham Nazmi <irkham.nazmi@gmail.com>
2023-06-06 07:14:50 +0700

    menambahkan kode style di theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor = const Color(0xffFD9A3F);
+Color backgroundColor2 = const Color(0xffFFFFFF);
+Color primaryTextColor = const Color(0xff000000);
+Color cardColor1 = const Color(0xff00FF00);
+Color cardColor2 = const Color(0xffFFFF00);
+Color cardColor3 = const Color(0xffFF0000);
+
+TextStyle primaryTextStyle =
+    GoogleFonts.notoSansThaiLooped(color: primaryTextColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+double extraSmall = 10.0;
+double small = 12.0;
+double large = 16.0;
+double extralarge = 18.0;
