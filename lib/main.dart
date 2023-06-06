651877c207ab0dfeb389b90356932189584263ca
Irkham Nazmi <irkham.nazmi@gmail.com>
2023-06-06 07:19:09 +0700

    slicing desain halaman monitoring suhu tubuh

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:temperatur_app/mobile_design_widget.dart';
+import 'package:temperatur_app/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,39 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Temp',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Monitoring Suhu Tubuh'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -49,70 +31,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? temp;
+  String? status;
+  Color? colorStatus;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    getInit();
+
+    super.initState();
+  }
+
+  getInit() async {
+    for (var i = 1; i < 4; i++) {
+      await Future.delayed(const Duration(seconds: 2))
+          .then((value) => getTemp(i));
+    }
+  }
+
+  getTemp(var i) {
+    switch (i) {
+      case 1:
+        setState(() {
+          temp = '33';
+          status = 'Normal';
+          colorStatus = cardColor1;
+        });
+
+        break;
+      case 2:
+        setState(() {
+          temp = '36.5';
+          status = 'Dalam Pantauan';
+          colorStatus = cardColor2;
+        });
+        break;
+      case 3:
+        setState(() {
+          temp = '40';
+          status = 'Segera ambil tindakan';
+          colorStatus = cardColor3;
+        });
+        break;
+      default:
+        setState(() {
+          temp = '33';
+          status = 'Normal';
+          colorStatus = cardColor1;
+        });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return MobileDesignWidget(
       child: Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-      ),
+          backgroundColor: backgroundColor,
+          body: Container(
+            margin:
+                const EdgeInsets.only(top: 39, right: 7, left: 7, bottom: 31),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    'Monitoring Suhu Tubuh',
+                    style: primaryTextStyle.copyWith(
+                        fontWeight: semiBold, fontSize: large),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(large),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: backgroundColor2),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Temperature',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: extralarge,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 34,
+                      ),
+                      Text(
+                        '${temp ?? '33'}  °C',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 50,
+                          fontWeight: bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 34,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 63,
+                        decoration:
+                            BoxDecoration(color: colorStatus ?? cardColor1),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 22, vertical: 21),
+                  child: Text(
+                    'Keterangan : ${status ?? 'Normal'}',
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
